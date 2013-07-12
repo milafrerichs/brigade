c1e5b2d9761f7fe1f82413b77cf61e94342113f2
Mila Frerichs <mila.frerichs@gmail.com>
2013-07-12 09:57:35 +0200

    win edit and show added

--- app/controllers/wins_controller.rb
+++ app/controllers/wins_controller.rb
@@ -6,9 +6,24 @@ class WinsController < ApplicationController
     
   end
   
+  def show
+    @win = Win.find(params[:id])
+  end
+  
   def edit
     @win = Win.find(params[:id])
     @location = Location.new
+  end
+  
+  def update
+    @win = Win.find(params[:id])
+    @win.update_attributes(params[:win])
+    if @win.save
+      flash[:success] = ""
+      redirect_to win_path(@win.id)
+    else
+      flash[:error] = ""
+    end
   end
 
 end
